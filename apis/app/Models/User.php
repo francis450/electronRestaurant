6b04859801b2d7bdc3c431e061d03163d73f3a49
Francis Kamande <franciskamande2001@gmail.com>
2024-01-17 23:26:50 +0300

    Add more properties to user schema

--- apis/app/Models/User.php
+++ apis/app/Models/User.php
@@ -18,8 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'fullname',
+        'username',
+        'phone',
         'email',
+        'role',
         'password',
     ];
 
@@ -42,4 +45,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // Get users roles
+    public function role($id)
+    {
+        return $this->where('id', $id)->first()->role;
+    }
+
+    // Get users username and password for authenticatio
 }
