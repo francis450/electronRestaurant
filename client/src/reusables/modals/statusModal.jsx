59ddb8aff2884547b00724b75913d1d86d9e49ff
Ivan Bowen <bowenivan16@gmail.com>
2024-01-19 01:34:57 +0300

    add StatusModal

---
+++ client/src/reusables/modals/statusModal.jsx
@@ -0,0 +1,68 @@
+// create a fixed top modal to display status messages - position fixed (top center)
+
+import { useEffect, useState } from "react";
+import { CloseStatusModalIcon } from "../svgs/svgs";
+
+function StatusModal({ statusData, setStatusData }) {
+  const [show, setShow] = useState(true);
+  const [color, setColor] = useState("");
+
+  const closeStatusModal = () => {
+    setShow(false);
+    setStatusData({
+      status: false,
+      message: "",
+      type: "",
+    });
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(false);
+      setStatusData({
+        status: false,
+        message: "",
+        type: "",
+      });
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, [statusData, setStatusData]);
+
+  useEffect(() => {
+    console.log("statusData", statusData);
+    if (statusData.type === "success") {
+      console.log("success");
+      setColor("green-700");
+    } else if (statusData.type === "error") {
+      console.log("error");
+      setColor("red-700");
+    } else if (statusData.type === "warning") {
+      setColor("yellow-700");
+    } else if (statusData.type === "info") {
+      setColor("blue-700");
+    }
+  }, [statusData]);
+
+  return (
+    <>
+      {show && (
+        <div className="animate-bounce status-modal fixed top-8 left-0 right-0 flex justify-center items-center">
+          <div
+            className={`status-modal-content relative w-[400px] p-2 rounded-md bg-[#D9D9D9] opacity-95 outline outline-${color}`}
+          >
+            <div
+              className={`absolute top-0.5 right-1 text-${color}`}
+              onClick={closeStatusModal}
+            >
+              <CloseStatusModalIcon className="w-5 h-5" />
+            </div>
+            <p className={`text-start text-${color}`}>Success Majamaa</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default StatusModal;
