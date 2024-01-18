fb357a21f83b4012dc0a7561037e18fc2b6d85b9
Ivan Bowen <bowenivan16@gmail.com>
2024-01-19 01:33:23 +0300

    create status modal context

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,24 +1,28 @@
-import logo from '../../logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import { Login } from "../../pages/login";
+import "./App.css";
+import StatusModal from "../../reusables/modals/statusModal";
+
+export const StatusModalContext = createContext();
 
 function App() {
+  const [statusData, setStatusData] = useState({
+    status: false,
+    message: "",
+    type: "",
+  });
+
+  console.log(statusData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StatusModalContext.Provider value={{ statusData, setStatusData }}>
+      <div className="App bg-[#282c34] min-h-screen">
+        {statusData.status && (
+          <StatusModal statusData={statusData} setStatusData={setStatusData} />
+        )}
+        <Login />
+      </div>
+    </StatusModalContext.Provider>
   );
 }
 
