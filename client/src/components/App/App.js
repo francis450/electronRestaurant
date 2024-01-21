40c5af6e1958281c4991d5a308d63e5629639a84
Ivan Bowen <bowenivan16@gmail.com>
2024-01-21 10:18:26 +0300

    configure routes

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,8 +3,27 @@ import { Login } from "../../pages/login";
 import "./App.css";
 import StatusModal from "../../reusables/modals/statusModal";
 import Dashboard from "../../pages/dashboard";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PrivateRoutes from "../../reusables/routes/PrivateRoutes";
 
 export const StatusModalContext = createContext();
+export const AuthContext = createContext();
+
+let router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    element: <PrivateRoutes />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
 
 function App() {
   const [statusData, setStatusData] = useState({
@@ -13,14 +32,21 @@ function App() {
     type: "",
   });
 
+  const [auth, setAuth] = useState({
+    id: null,
+    user: "",
+    token: "",
+  });
+
   return (
     <StatusModalContext.Provider value={{ statusData, setStatusData }}>
       <div className="App bg-[#282c34] min-h-screen">
         {statusData.status && (
           <StatusModal statusData={statusData} setStatusData={setStatusData} />
         )}
-        <Login />
-        <Dashboard />
+        <AuthContext.Provider value={{ auth, setAuth }}>
+          <RouterProvider router={router} />
+        </AuthContext.Provider>
       </div>
     </StatusModalContext.Provider>
   );
