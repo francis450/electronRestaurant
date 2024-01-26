ebe92b5aadef397d47ebbfc2d73c8789422802bb
Ivan Bowen <bowenivan16@gmail.com>
2024-01-26 13:10:55 +0300

    add height props

--- client/src/reusables/modals/defaultModal.jsx
+++ client/src/reusables/modals/defaultModal.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { CloseStatusModalIcon } from "../svgs/svgs";
 
-export const DefaultModal = ({ children, header, closeModal }) => {
+export const DefaultModal = ({ children, header, closeModal, height=55 }) => {
   return (
     <div className="flex fixed top-0 right-0 bottom-0 left-0 justify-center bg-black bg-opacity-30 items-center">
-      <div className="modal rounded-md w-2/5 h-[55vh] bg-white">
+      <div className={`modal rounded-md w-2/5 h-[${height}vh] bg-white`} style={{height: `${height}vh`}}>
         <div className="modal-header h-13 bg-slate-700 flex items-center justify-between p-3">
           <h3 className="font-medium text-lg text-white">{header}</h3>
           <div className="close-modal" onClick={() => closeModal()}>
