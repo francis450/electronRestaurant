b4859ffd51d7df837ea843bb083fa0684a6387f9
Ivan Bowen <bowenivan16@gmail.com>
2024-01-30 22:18:22 +0300

    add receipts tab

--- client/src/pages/inventory.jsx
+++ client/src/pages/inventory.jsx
@@ -3,6 +3,7 @@ import { AuthContext } from "../components/App/App";
 import Authenticated from "../reusables/layouts/AuthenticaedLayout";
 import Suppliers from "../components/Inventory/suppliers/Suppliers";
 import Items from "../components/Inventory/items/Items";
+import Receipts from "../components/Inventory/receipts/Receipts";
 
 const Inventory = () => {
   const { auth } = useContext(AuthContext);
@@ -13,7 +14,7 @@ const Inventory = () => {
       header={<h1 className="text-2xl text-white">Inventory</h1>}
     >
       {/* tabs */}
-      <div className="grid grid-cols-2 text-[#222] rounded-xl">
+      <div className="grid grid-cols-3 text-[#222] rounded-xl">
         <span
           className={`col-span-1 py-2 text-md  ${
             tab === 1 ? "bg-[whitesmoke]" : "bg-[#3a3f49]  text-[whitesmoke]"
@@ -30,9 +31,18 @@ const Inventory = () => {
         >
           Suppliers
         </span>
+        <span
+          className={`col-span-1 py-2 text-md ${
+            tab === 3 ? "bg-[whitesmoke]" : "bg-[#3a3f49] text-[whitesmoke]"
+          }`}
+          onClick={() => setTab(3)}
+        >
+          Receipts
+        </span>
       </div>
       {tab === 1 && <Items />}
       {tab === 2 && <Suppliers />}
+      {tab === 3 && <Receipts />}
     </Authenticated>
   );
 };
