ae788e85b2977dc449d19ff0512240b38f8d41b9
Ivan Bowen <bowenivan16@gmail.com>
2024-02-14 09:12:55 +0300

    add open menu and close menu svgs

--- client/src/reusables/svgs/svgs.jsx
+++ client/src/reusables/svgs/svgs.jsx
@@ -206,3 +206,41 @@ export function ReceiptIcon({ className = sizeClass }) {
     </svg>
   );
 }
+
+export function CloseMenubar({ className = sizeClass }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="white"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18 18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
+
+export function OpenMenubar({ className = sizeClass }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="white"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+      />
+    </svg>
+  );
+}
