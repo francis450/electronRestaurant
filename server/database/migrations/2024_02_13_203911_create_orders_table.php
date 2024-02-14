08d9c61faf46f3dd5c06d18a20f5874718454f7a
Francis Kamande <franciskamande2001@gmail.com>
2024-02-14 17:32:43 +0300

    update orders migration, and update payments migration

--- server/database/migrations/2024_02_13_203911_create_orders_table.php
+++ server/database/migrations/2024_02_13_203911_create_orders_table.php
@@ -15,13 +15,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users','id');
             $table->foreignId('table_id')->constrained('tables','id');
-            $table->foreignId('payment_id')->constrained('payment', 'id');
             $table->enum('status',['pending','completed','canceled'])->default('pending');
             $table->double('total_price', 8, 2);
             $table->text('note')->nullable();
             $table->timestamp('completed_at')->nullable();
             $table->timestamp('canceled_at')->nullable();
-            $table->softDeletes();
             $table->timestamps();
         });
     }
